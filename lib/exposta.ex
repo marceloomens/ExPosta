23295421d9c965eb87e184109bc9d2ed768cc75c
Marcel Oomens <mail@marceloomens.com>
2015-07-20 08:27:01 +0200

    Implement response processing
    Signed-off-by: Marcel Oomens <mail@marceloomens.com>

--- lib/exposta.ex
+++ lib/exposta.ex
@@ -1,11 +1,27 @@
 defmodule ExPosta do
 
   @endpoint Application.get_env(:postmark, :api_endpoint)
+
   @headers [
     "Accept": "application/json",
     "Content-Type": "application/json",
     "X-Postmark-Server-Token": Application.get_env(:postmark, :server_token),
   ]
+
+  defp process(response) do
+    {:ok, body} = JSX.decode(response.body)
+    case response.status_code do
+      200 ->
+        {:ok, body["Message"]}
+      401 ->
+        {:error, body["Message"]}
+      422 ->
+        # Unprocessible entity needs further inspection
+        {:error, body["Message"]}
+      500 ->
+        {:error, body["Message"]}
+    end
+  end
 
   # Do I need several strategies, including async, await, etc?
   def send(to, subject, textBody, htmlBody) do
@@ -16,8 +32,8 @@ defmodule ExPosta do
       "textBody"  => textBody,
       "htmlBody"  => htmlBody
     }
-    response = HTTPotion.post(@endpoint, [body: json, headers: @headers])
-    IO.inspect response
+    HTTPotion.post(@endpoint, [body: json, headers: @headers])
+    |> process
   end
-  
+
 end
